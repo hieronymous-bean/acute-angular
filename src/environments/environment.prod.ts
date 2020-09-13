1f4224750b6d3608d5e2ef8e58cf181f85e246a2
Jeffrey Allan Brown <jeffreybrown7193@gmail.com>
2020-09-12 21:41:27 -0400

    firebase config and basic setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCLH1kxVFbGl-P4xmQohwmTmwetmIEjqFU",
+    authDomain: "acute-angular.firebaseapp.com",
+    databaseURL: "https://acute-angular.firebaseio.com",
+    projectId: "acute-angular",
+    storageBucket: "acute-angular.appspot.com",
+    messagingSenderId: "791620895593",
+    appId: "1:791620895593:web:ad9f10840b9cd077f7f3f8",
+    measurementId: "G-KSLD1YR86N"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCLH1kxVFbGl-P4xmQohwmTmwetmIEjqFU",
+    authDomain: "acute-angular.firebaseapp.com",
+    databaseURL: "https://acute-angular.firebaseio.com",
+    projectId: "acute-angular",
+    storageBucket: "acute-angular.appspot.com",
+    messagingSenderId: "791620895593",
+    appId: "1:791620895593:web:ad9f10840b9cd077f7f3f8",
+    measurementId: "G-KSLD1YR86N"
+  }
 };
 
 /*
